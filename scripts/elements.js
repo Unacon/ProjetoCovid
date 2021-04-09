62d3712ec9a7064188e038c2eefa939d1f88d191
Joao Vitor <joaovitor_892@hotmail.com>
2021-04-08 22:59:15 -0300

    Finalizado v1.0

--- scripts/elements.js
+++ scripts/elements.js
@@ -14,15 +14,46 @@ export const resultStateElement = document.querySelector('#resultState');
 export const spanElement = document.querySelector('#teste');
 export const stateElement = document.querySelector('#state');
 
-//Cria as opções das cidades 
-export function creatOption(states){
+export const CountryElement = document.querySelector('#country');
+export const countryNameElement = document.querySelector('.countryName');
+
+export const nameCountryElement = document.querySelector('#nameCountry');
+export const dateResultElement = document.querySelectorAll('.dateResult');
+
+export const WorldDateElement = document.querySelectorAll('.WorldDate');
+
+
+
+//LIMPAS AS OPTIONS DOS ESTADOS
+export function optionToClean(){
+  let valor = stateElement.length;
+  for(let i=0;i<valor;i++){        
+    stateElement.removeChild(stateElement.childNodes[0])
+  };
+}
+
+//Cria as opções dos estados
+export function creatOptionState(states){  
+  optionToClean();
   states.forEach((State) => {
-    let newOption = document.createElement('option');
+    let newOption = document.createElement('option');   
     newOption.value = State.provinceState;
-    newOption.text = State.provinceState;
-    stateElement.add(newOption);
+    newOption.text = State.provinceState;    
+    stateElement.add(newOption)
   });
 };
+
+//Cria as opções dos paises 
+export function creatOptionCountry(countries){  
+  countries.forEach((country) => {
+    let newOption = document.createElement('option');   
+    newOption.value = country.iso3;
+    newOption.text = country.name;
+    newOption.className = 'countryName';
+    CountryElement.add(newOption);    
+  });
+};
+
 
 //LIMPA DADOS DA TELA
 export function toClean(){
@@ -31,6 +62,8 @@ export function toClean(){
     resultDate.innerHTML = '';
   });
   resultDateElement.innerHTML = '';
+  dateResultElement.forEach((result) => result.style.display = 'none');
+ 
 };
 
 //AUTENTICA ENTRADAS
@@ -48,18 +81,22 @@ export function authenticateInput(){
 };
 
 //Mostra os dados que o cliente deseja no front-end
-export function outputDate(stateResult){
-  stateResult.forEach((state) => {                
-    resultStateElement.innerHTML = `${state.provinceState} :`;// Estado
-
+export function outputDate(stateResult){     
+  dateResultElement.forEach((result) => result.style.display = 'block');             
+    resultStateElement.innerHTML = `${stateResult.provinceState} `;// Estado
     dateElement.forEach((box,key) =>{
       //Mostra somente os dados selecionados
-      if(box.checked){                          
-        resultDateElement[key].innerHTML = `- ${resultDateElement[key].id}: ${state[box.id].toLocaleString()}`;                      
+      
+      
+      if(box.checked){   
+              
+        if(stateResult[box.id] === null){          
+          resultDateElement[key].innerHTML = `- ${resultDateElement[key].id}: Sem informações`;                  
+        }else{               
+          resultDateElement[key].innerHTML = `${stateResult[box.id].toLocaleString()}`;                      
+        }
       };
     });
-
-  });
 };
 
 //EVENTO DO BOTÃO "RESETAR"
@@ -71,4 +108,9 @@ resetElement.addEventListener('click', () =>{
   });  
   toClean();
 });
+
+//SETA TITULO DOS RESULTADO DA PESQUISA
+dateResultElement[0].insertAdjacentHTML('afterbegin',`Confirmed`);
+dateResultElement[1].insertAdjacentHTML('afterbegin',`Recovered`);
+dateResultElement[2].insertAdjacentHTML('afterbegin',`Deaths`);
 
--- scripts/index.js
+++ scripts/index.js
@@ -2,38 +2,68 @@ import * as ElemFunc from './elements.js';
 
 // GET DADOS
 ///GET DADOS PAIS
-async function getDateCountry(){
-  const response = await fetch('https://covid19.mathdro.id/api/countries/BRA');
+async function getDateWorld(){
+  const response = await fetch(`https://covid19.mathdro.id/api`);
+  const json = await response.json();
+  return json;
+}
+
+async function getDateCountry(id = ''){
+  const response = await fetch(`https://covid19.mathdro.id/api/countries/${id}`);
   const json = await response.json();
   return json;
 }
 
 ///GET DADOS ESTADOS
-async function getDateProvinceState(){
-  const response = await fetch('https://covid19.mathdro.id/api/countries/BR/confirmed');
+async function getDateProvinceState(id){
+  const response = await fetch(`https://covid19.mathdro.id/api/countries/${id}/confirmed`);
   const json = await response.json();
   return json;
 }
 
 async function run(){
-  //coleta dados da API-COVID
-  const resultCountry = await getDateCountry();
-  const states = await getDateProvinceState();
+  ElemFunc.toClean();// LIMPA A TELA
+   
+  const country = await getDateCountry(); // DADOS DOS PAISES
+  //Dado do Mundo e setando no front-end
+  const world = await getDateWorld();
+  ElemFunc.WorldDateElement[0].innerHTML = `${world.confirmed.value.toLocaleString()}`;
+  ElemFunc.WorldDateElement[1].innerHTML = `${world.deaths.value.toLocaleString()}`;
+  ElemFunc.WorldDateElement[2].innerHTML = `${world.recovered.value.toLocaleString()}`;
+
+
+
+  //Cria as opções dos paises
+  ElemFunc.creatOptionCountry(country.countries);  
+
+  //EVENTO QUANDO É SELECIONADO ALGUM PAIS
+  ElemFunc.CountryElement.addEventListener('change', async () => {     
+    const countryName = ElemFunc.CountryElement.value;  
+    
+    //SETA OS ESTADOS NO FRONT PARA ESCOLHA
+    const states = await getDateProvinceState(countryName); 
+    ElemFunc.creatOptionState(states); 
+
+    
+    const resultCountry = await getDateCountry(countryName);
+    
+    //SETA OS DADOS DO PAIS NO FRONT-END
+    ElemFunc.nameCountryElement.innerHTML = `${ElemFunc.CountryElement.value}`;
+    ElemFunc.countryDate[0].innerHTML = `${resultCountry.confirmed.value.toLocaleString()}`;
+    ElemFunc.countryDate[1].innerHTML = `${resultCountry.recovered.value.toLocaleString()}`;
+    ElemFunc.countryDate[2].innerHTML = `${resultCountry.deaths.value.toLocaleString()}`;       
+  });
   
-  //.toLocaleString() => Sensivel ao numeros
-  //Dados do Brasil
-  ElemFunc.countryDate[0].innerHTML = `- Confirmados: ${resultCountry.confirmed.value.toLocaleString()}`;
-  ElemFunc.countryDate[1].innerHTML = `- Recuperados: ${resultCountry.recovered.value.toLocaleString()}`;
-  ElemFunc.countryDate[2].innerHTML = `- Falecidos: ${resultCountry.deaths.value.toLocaleString()}`;
   
-  //Cria as opções das cidades 
-  ElemFunc.creatOption(states);
 
   //EVENTO DO BOTÃO 'CONFIRMAR'
-  ElemFunc.buttonElement.addEventListener('click', () =>{
+  ElemFunc.buttonElement.addEventListener('click', async () =>{
+    //Nome do estado
+    const states = await getDateProvinceState(ElemFunc.CountryElement.value); 
+    
     //Limpa dados da tela
     ElemFunc.toClean();
-
+    
     //AUTENTICADOR DE ENTRADAS
     const authenticate = ElemFunc.authenticateInput();
     
@@ -42,11 +72,12 @@ async function run(){
       //Filtra os estados que o cliente deseja
       let stateResult = states.filter((state) => state.provinceState === ElemFunc.stateElement.value);
 
-      //Mostra os dados que o cliente deseja no front-end
-      ElemFunc.outputDate(stateResult);      
+      //Mostra os dados que o cliente deseja no front-end      
+      ElemFunc.outputDate(stateResult[0]);      
     }
     else{
       alert('Selecione uma cidade e pelo menos um campo');
+      
     }
   }); 
 
@@ -56,4 +87,5 @@ run();
 
 
 // var projection = d3.geoNaturalEarth2();
-console.log(d3.geoPath());
+// console.log(d3.geoPath());
+//window.location.href = "http://pt.stackoverflow.com"; MUDAR DE PAGINA
